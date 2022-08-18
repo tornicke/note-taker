161cfe73666be84d26b69bd41479e77b2045f063
tornicke <torniqe@gmail.com>
2022-08-18 21:50:30 +0200

    Incorporating the nodes.html into the app

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+const path = require("path");
+
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("public")); //serving the static resources
 
 //CODE Here
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}.`));
