bec64a5fa5667b21d83775c145eee9b95f182a62
tornicke <torniqe@gmail.com>
2022-08-20 12:58:17 +0200

    Fixing the db.json display problem

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+);
+
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
